02938c5987b067cb0d93bbadf835b8109af2a4df
Blake DeGraw <bbdegraw@gmail.com>
2021-06-04 13:07:00 -0700

    oscC created, not tested

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
+
+  let playC, oscillatorC
+
+  let oscC = {
+    type: "triangle",
+    frequency: 262,
+    playing: false
+  }
+
+  const audioContext = new AudioContext()
+
+  window.onload = function(){
+    playC = function(){
+      if(oscC.playing){
+        oscillatorC.stop()
+        oscC.playing = false
+      } else{
+        oscillatorC = audioContext.createOscillator()
+        oscillatorC.type = oscC.type
+        oscillatorC.frequency.setValueAtTime(oscC.frequency, audioContext.currentTime)
+        oscillatorC.connect(audioContext.destination)
+        oscillatorC.start()
+        oscC.playing = true
+      }
+    }
+  }
+
   return (
     <div className="App">
       <h1>sanity check</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
